ed6e3c2ee01dd4ea406a9011b47b1c181c1ac2a9
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-10-17 18:04:46 +0300

    controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get , Param} from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+    @Get()
+    getAll(){
+        return 'getAll'
+    }
+
+    @Get(':id')
+    getOne(@Param() params): string{
+        return 'getone' + params.id
+    }
+}
