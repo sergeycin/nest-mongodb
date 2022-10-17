31ef30a6078595cb8649cbd53e9f06ee6d1f9438
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-10-17 21:07:41 +0300

    service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,12 @@
-import { Controller,Get , Param} from '@nestjs/common';
+import { Body, Controller,Delete,Get , Header, HttpCode, HttpStatus, Param, Post, Put, Redirect} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
 
     @Get()
+    @Redirect('https://google.com', 301)
     getAll(){
         return 'getAll'
     }
@@ -11,5 +14,21 @@ export class ProductsController {
     @Get(':id')
     getOne(@Param() params): string{
         return 'getone' + params.id
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Cache-Control', 'none')
+    create(@Body() createProductDto: CreateProductDto){
+        return `Title ${createProductDto.title}`
+    }
+
+    @Delete(':id')
+    remove(@Param(':id') params): string{
+        return 'Remove'  
+    }
+    @Put(':id')
+    update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string){
+        return 'Update' + id
     }
 }
