093b05ec027743e0c521fd1d02e473e408351e0d
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-10-18 17:41:49 +0300

    products ready

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,34 +1,40 @@
 import { Body, Controller,Delete,Get , Header, HttpCode, HttpStatus, Param, Post, Put, Redirect} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
 
+    constructor(private readonly productService: ProductsService){
+
+    }
+
     @Get()
     @Redirect('https://google.com', 301)
-    getAll(){
-        return 'getAll'
+    getAll(): Promise<Product[]>{
+        return this.productService.getAll()
     }
 
     @Get(':id')
-    getOne(@Param() params): string{
-        return 'getone' + params.id
+    getOne(@Param('id') id: string): Promise<Product>{
+        return this.productService.getById(id)
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @Header('Cache-Control', 'none')
-    create(@Body() createProductDto: CreateProductDto){
-        return `Title ${createProductDto.title}`
+    create(@Body() createProductDto: CreateProductDto): Promise<Product>{
+        return this.productService.create(createProductDto)
     }
 
     @Delete(':id')
-    remove(@Param(':id') params): string{
-        return 'Remove'  
+    remove(@Param(':id') id: string): Promise<Product>{
+        return this.productService.remove(id)
     }
     @Put(':id')
-    update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string){
-        return 'Update' + id
+    update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): Promise<Product>{
+        return this.productService.update(id,updateProductDto)
     }
 }
